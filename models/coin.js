56fb501dec66abdbdfe4bc286ef77171638325e3
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-03-07 22:12:15 -0700

    Created file

---
+++ models/coin.js
@@ -0,0 +1,178 @@
+const coins = [
+    {
+        name: "Eisenhour Dollar", 
+        denomination: "One Dollar",
+        nickname: "Ike",
+        year: 1974,
+        worth: 2,
+        tags: ["nothing unique"]
+    },
+    {
+        name: "Eisenhour Dollar", 
+        denomination: "One Dollar",
+        nickname: "Bi-Centennial",
+        year: 1976,
+        worth: 2,
+        tags: ["discolored", "copper=like coloring"]
+    },   
+    {
+        name: "Eisenhour Dollar", 
+        denomination: "One Dollar",
+        nickname: "Ike",
+        year: 1978,
+        worth: 2,
+        tags: ["good conditions"]
+    }, 
+    {
+        name: "Eisenhour Dollar", 
+        denomination: "One Dollar",
+        nickname: "Ike",
+        year: 1974,
+        worth: 2,
+        tags: ["very defined"]
+    },
+    {
+        name: "Eisenhour Dollar", 
+        denomination: "One Dollar",
+        nickname: "Ike",
+        year: 1974,
+        worth: 2,
+        tags: ["Variety 2"]
+    },
+    {
+        name: "Eisenhour Dollar", 
+        denomination: "One Dollar",
+        nickname: "Bicentennial",
+        year: 1976,
+        worth: 1,
+        tags: ["Alaska flag painting"]
+    },
+    {
+        name: "Indian Head", 
+        denomination: "Penny",
+        nickname: "None",
+        year: 1903,
+        worth: 3,
+        tags: ["faded", "set"]
+    },
+    {
+        name: "Indian Head", 
+        denomination: "Penny",
+        nickname: "None",
+        year: 1900,
+        worth: 2,
+        tags: ["faded"]
+    },
+    {
+        name: "Indian Head", 
+        denomination: "Penny",
+        nickname: "None",
+        year: 1902,
+        worth: 2,
+        tags: ["faded"]
+    },
+    {
+        name: "Indian Head", 
+        denomination: "Penny",
+        nickname: "None",
+        year: 1903,
+        worth: 2,
+        tags: ["faded"]
+    },
+    {
+        name: "Indian Head", 
+        denomination: "Penny",
+        nickname: "None",
+        year: 1905,
+        worth: 2,
+        tags: ["faded"]
+    },
+    {
+        name: "Indian Head", 
+        denomination: "Penny",
+        nickname: "None",
+        year: 1901,
+        worth: 2,
+        tags: ["more defined"]
+    },
+    {
+        name: "Lincoln", 
+        denomination: "Penny",
+        nickname: "None",
+        year: 1929,
+        worth: .2,
+        tags: ["more defined", "set"]
+    },
+    {
+        name: "Liberty Head", 
+        denomination: "Nickle",
+        nickname: "None",
+        year: 1901,
+        worth: 2,
+        tags: ["very faded", "G4 or Lower"]
+    },
+    {
+        name: "Liberty Head", 
+        denomination: "Nickle",
+        nickname: "None",
+        year: 1907,
+        worth: 2,
+        tags: ["very faded"]
+    },
+    {
+        name: "Liberty Head", 
+        denomination: "Nickle",
+        nickname: "None",
+        year: 1912,
+        worth: 2,
+        tags: ["G4 or Lower"]
+    },
+    {
+        name: "Liberty Head", 
+        denomination: "Nickle",
+        nickname: "None",
+        year: 1903,
+        worth: 2,
+        tags: ["G4 or Lower"]
+    },
+    {
+        name: "Wartime Jefferson", 
+        denomination: "Nickle",
+        nickname: "None",
+        year: 1943,
+        worth: 2.5,
+        tags: ["G4 or Lower", "set"]
+    },
+    {
+        name: "Buffalo Head", 
+        denomination: "Nickle",
+        nickname: "Indian Head",
+        year: 1930,
+        worth: 1.25,
+        tags: ["P", "set"]
+    },
+    {
+        name: "Silver Mercury", 
+        denomination: "Dime",
+        nickname: "None",
+        year: 1944,
+        worth: 2.75,
+        tags: ["W", "set"]
+    },
+    {
+        name: "Bicentennial Washington", 
+        denomination: "Quarter",
+        nickname: "None",
+        year: 1976,
+        worth: 1.25,
+        tags: ["none", "set"]
+    },
+    {
+        name: "JFK Bicentennial", 
+        denomination: "Half Dollar",
+        nickname: "None",
+        year: 1976,
+        worth: 3,
+        tags: ["very clear", "set"]
+    },
+]
